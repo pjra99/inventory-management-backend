8567c2be3bd60af8dee32492faba8c507fb1ad3b
Pranjal Rai <raipranjalofficial@gmail.com>
2025-03-14 00:01:47 +0530

    --added: customers and users apis

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config({path: 'C:\\Users\\pranj\\b-projects\\inventory-management-backend\\.env'})
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI)
-
+console.log("Conneted to Mongodb")
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -14,55 +14,57 @@ app.get('/', (req, res)=>{
     }).status(200)
 })
 
-app.get('/users', async (req, res) => {
-    try {
-        const users = await mongoose.connection.db.collection('users').find().toArray();
-        res.status(200).json(users);
+app.all('/users', async (req, res) => {
+    // console.log(req.method)
+    if (req.method==="GET"){
+        try {
+            const users = await mongoose.connection.db.collection('users').find().toArray();
+            res.status(200).json(users);
+            }
+     
+     catch (e) {
+        console.error("Error fetching users:", e);
+        res.status(500).json({ error: "Internal Server Error" });
         }
- 
- catch (e) {
-    console.error("Error fetching users:", e);
-    res.status(500).json({ error: "Internal Server Error" });
+    }
+    else if (req.method==="POST")
+        {
+        try{
+            const users = await mongoose.connection.db.collection('users')
+            const result = req.body.length? await users.insertMany(req.body):await users.insertOne(req.body)
+            res.status(200).json(result)
+        }
+        catch(e){
+            res.status(400).json({"Error inserting user(s)":e})
+        }
     }
 });
 
-app.post('/users', async(req, res)=>{
-try{
-    const users = await mongoose.connection.db.collection('users')
-    const result = await users.insertOne(req.body)
-    res.status(200).json(result)
-}
-catch(e){
-    res.status(500).json({
-        "err":{e}
-    })
-}
-})
-
-app.post('/customers', async(req, res)=>{
-    try{
-        const customers = await mongoose.connection.db.collection('customers')
-        const result = await customers.insertOne(req.body)
-        res.status(200).json(result)
+app.all('/customers', async(req, res)=>{
+    if(req.method==="GET"){
+        try {
+            const customers = await mongoose.connection.db.collection('customers').find().toArray();
+            res.status(200).json(customers);
+            }
+     
+     catch (e) {
+        res.status(500).json({ "Err": e });
+        }
     }
-    catch(e){
-        res.status(500).json({
-            "err":{e}
-        })
+    else if(req.method==="POST") {
+        try{
+            const customers = await mongoose.connection.db.collection('customers')
+            const result = req.body.length? await customers.insertMany(req.body):await customers.insertOne(req.body)
+            res.status(200).json(result)
+        }
+        catch(e){
+            res.status(400).json({"Error inserting user(s)":e})
+        }
     }
+    
     }) 
 
-app.get('/customers', async (req, res) => {
-    try {
-        const users = await mongoose.connection.db.collection('customers').find().toArray();
-        res.status(200).json(users);
-        }
-    catch (e) {
-    console.error("Error fetching customers:", e);
-    res.status(500).json({ error: {e} });
-    }
-});
-   
+
 
 const port = process.env.PORT || '4000'
 app.listen(port, ()=>{
