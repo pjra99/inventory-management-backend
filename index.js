1e9a1947d576b4477e5b86bfa26b36547a67589a
Pranjal Rai <raipranjalofficial@gmail.com>
2025-03-23 18:55:58 +0530

    --added: date range filter in products api

--- index.js
+++ index.js
@@ -157,33 +157,30 @@ app.all('/:org_id/customers', async(req, res)=>{
 
     //products api
     app.all('/:org_id/products/:product_category?/:start_date?/:end_date?', async(req, res)=>{
+        const products_collection = await mongoose.connection.db.collection('products')
         if(req.method==="GET"){
-            let product_category = req.params['product_category']
+            // let product_category = req.params['product_category']
             let start_date = req.params["start_date"]
             let end_date = req.params["end_date"]
-            if(product_category && start_date && end_date){
-                try{
-                    const products = await mongoose.connection.db.collection('products').find({
-                        "category": product_category,
-                        "query": {$gte: new Date(start_date), $lte: new Date(end_date)}
-                    }).toArray()
-                    res.status(200).json(products)
-                }
-                catch(e){
-                    res.status(500).json({ "err": e.message });
-                }
+            let product_category = req.params["product_category"]
+            filter = {}
+            if(product_category!=="all"){
+            filter.category = product_category
             }
-            else{
-                try {
-                    console.log(req.params['product_category'])
-                    const products = await mongoose.connection.db.collection('products').find().toArray();
-                    res.status(200).json(products);
-                    }
-             
+            if(start_date && end_date){
+                filter.date_added = end_date? {$gte: start_date, $lte: end_date}:{$gte: start_date}
+            }
+            console.log(filter)
+        try {
+            console.log(req.params['product_category'])
+            const products = await products_collection.find(filter).toArray();
+            res.status(200).json(products);
+            }
+        
              catch (e) {
                 res.status(500).json({ "err": e.message });
                 }
-            }
+            
         }
         else if(req.method==="POST") {
             let org_id = req.params["org_id"]
@@ -192,8 +189,7 @@ app.all('/:org_id/customers', async(req, res)=>{
                     ...key,
                     "org_id": org_id
                 })):{...req.body,"org_id": org_id}
-                const products = await mongoose.connection.db.collection('products')
-                const response = await Array.isArray(req.body)? await products.insertMany(req.body):await products.insertOne(req.body)
+                const response = await Array.isArray(req.body)? await products_collection.insertMany(req.body):await products.insertOne(req.body)
                 console.log(response)
                 res.status(200).json(response)
             }
@@ -204,20 +200,37 @@ app.all('/:org_id/customers', async(req, res)=>{
         })
 
     app.all('/:org_id/stock_details', async(req, res)=>{
+        const stock_collection = await mongoose.connection.db.collection('stock_details')
     if (req.method==="GET"){
         try{
-            const stock_details= await mongoose.connection.db.collection('stock_details').find().toArray()
+            const stock_details= await stock_collection.find(
+                {"org_id": req.params["org_id"]}
+            ).toArray()
             res.status(200).json(stock_details)
         }
         catch(e){
             res.status(400).json({"err": e})
         }
     }
-    else if (method==="POST"){
+    else if (req.method==="POST"){
         try{
-            const stock_details = await mongoose.connection.db.collection('stock_details')
-            const response = Array.isArray(req.body)? await stock_details.insertOne(req.body): await stock_details.insertMany(req.body)
-            res.status(200).json(response)
+            if (Array.isArray(req.body)){
+                const payload = req.body.map((key)=>({
+                    
+                        ...key,
+                        "org_id": req.params["org_id"]
+                        
+                    
+                }))
+                const response = await stock_collection.insertMany(payload)
+                res.status(200).json(response)
+            }
+            else{
+              const response = await stock_collection.insertOne(req.body)
+              res.status(200).json(response)
+            }
+            
+            
         }
         catch(e){
            res.status(400).json({"err":e.message})
@@ -226,9 +239,10 @@ app.all('/:org_id/customers', async(req, res)=>{
     }) 
 
     app.all('/organisation', async(req, res)=>{
+        const organisations_collection = await mongoose.connection.db.collection('organisation')
         if (req.method==="GET"){
             try{
-                const organisation= await mongoose.connection.db.collection('organisation').find().toArray()
+                const organisation= await organisations_collection.find().toArray()
                 res.status(200).json(organisation)
             }
             catch(e){
@@ -237,8 +251,8 @@ app.all('/:org_id/customers', async(req, res)=>{
         }
         else if (method==="POST"){
             try{
-                const organisation = await mongoose.connection.db.collection('organisation')
-                const response = Array.isArray(req.body)? await organisation.insertOne(req.body): await organisation.insertMany(req.body)
+                
+                const response = Array.isArray(req.body)? await organisations_collection.insertOne(req.body): await organisations_collection.insertMany(req.body)
                 res.status(200).json(response)
             }
             catch(e){
